c1826aac2226fa947991fba3829e99dd39a0ff31
Kunj05 <kunjchandak05@gmail.com>
2025-02-02 16:51:00 +0530

    remove authentication middleware from payment routes for create-order and verifyPayment

--- backend/routes/payment.route.js
+++ backend/routes/payment.route.js
@@ -1,12 +1,7 @@
 import express from "express";
 const router = express.Router();
 
-// Importing Middlewares
-// const { auth } = require("../middlewares/Auth");
 import { createOrder, verifyPayment } from "../controllers/payment.controller.js";
-
-// router.post("/create-order", auth, createOrder);
-// router.post("/verifyPayment", auth, verifyPayment);
 
 router.post("/create-order", createOrder);
 router.post("/verifyPayment", verifyPayment);
