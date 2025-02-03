1db68cbfe59f88773ca483517eb0eb5c518a1902
Kunj05 <kunjchandak05@gmail.com>
2025-02-03 18:31:57 +0530

    update allowed origin in CORS configuration and add .env to .gitignore for Twilio backend

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ import  paymentRoutes from "./routes/payment.route.js"
 const app = express()
 
 app.use(cors({
-    origin: ['http://localhost:3000'], // Add localhost to the allowed origins
+    origin: ['http://localhost:5173'], // Add localhost to the allowed origins
     credentials: true,
 }));
  
